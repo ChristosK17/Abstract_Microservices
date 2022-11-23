f195feb4456e6813621caab0e5a761381c426015
ckiokak <christoskiokakis@gmail.com>
2022-11-23 23:51:00 +0200

    With Swagger

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
 from flask import Flask, redirect, url_for, request
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
-    }
-)
+CONFIG = {'app_name': "Abstract Microservices"}
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(SWAGGER_URL, API_URL, CONFIG)
 
 
 @app.route("/")
